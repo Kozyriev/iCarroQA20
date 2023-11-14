289b871aea35c5e1acb4f3255118820ec1d9ba65
Kozyriev <S.Kozurev@ukr.net>
2023-11-14 11:46:01 +0200

    Initial commit

---
+++ src/test/java/manager/ApplicationManager.java
@@ -0,0 +1,72 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.ConfigProperties;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+
+    Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
+
+  //  WebDriver driver;
+  EventFiringWebDriver driver;
+
+    UserHelper userHelper;
+    CarHelper carHelper;
+    String browser;
+    String url = ConfigProperties.getProperty("url");
+    public ApplicationManager() {
+        browser = System.getProperty("browser", BrowserType.CHROME);
+    }
+
+    public void init() {
+       // driver = new ChromeDriver();
+       // driver = new EventFiringWebDriver(new ChromeDriver());
+
+        if(browser.equals(BrowserType.CHROME)) {
+            driver = new EventFiringWebDriver(new ChromeDriver());
+            logger.info("created chrome browser");
+        }else if (browser.equals(BrowserType.FIREFOX)) {
+            driver = new EventFiringWebDriver(new FirefoxDriver());
+            logger.info("started tests in firefox driver");
+        }
+
+        driver.navigate().to(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.register(new WDListener());
+        userHelper = new UserHelper(driver);
+        carHelper = new CarHelper(driver);
+        logger.info("navigated to the url: " + url);
+    }
+
+    public void navigateToMainPage() {
+        driver.navigate().to(url);
+    }
+
+    public UserHelper getUserHelper() {
+        return userHelper;
+    }
+    public CarHelper getCarHelper() {
+        return carHelper;
+    }
+
+    public EventFiringWebDriver getDriver() {
+        if(driver == null) {
+            init();
+        }
+        return driver;
+    }
+
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,127 @@
+package tests;
+
+import data.DataProviderLogin;
+import dto.UserDTO;
+import dto.UserDTOWith;
+import dto.UserDtoLombok;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class LoginTests extends BaseTest{
+
+    @AfterMethod
+    public void postconditionsLogin() {
+        if(flagPopUp) {
+            flagPopUp = false;
+            app.getUserHelper().clickOkPopUpSuccessLogin();
+        }
+        if(flagLogin) {
+            flagLogin = false;
+            app.getUserHelper().logout();
+        }
+    }
+
+    @Test(priority = 1, invocationCount = 2)
+    public void positiveLoginUserDTO() {
+        UserDTO userDTO = new UserDTO("testqa20@gmail.com", "123456Aa$");
+        app.getUserHelper().login(userDTO);
+        flagLogin = true;
+        flagPopUp = true;
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterLogin());
+    }
+
+    @Test(groups={"smoke"})
+    public void positiveLoginUserDTOWith() {
+        UserDTOWith userDTOWith = new UserDTOWith()
+                .withEmail("testqa20@gmail.com")
+                .withPassword("123456Aa$");
+        app.getUserHelper().login(userDTOWith);
+        flagLogin = true;
+        flagPopUp = true;
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterLogin());
+    }
+
+    @Test(dataProvider = "loginCSV", dataProviderClass = DataProviderLogin.class, groups = {"smoke"})
+    public void positiveLogin(UserDtoLombok user) {
+        app.getUserHelper().loginUserDtoLombok(user);
+        flagLogin = true;
+        flagPopUp = true;
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterLogin());
+    }
+
+    @Test(dataProvider = "positiveDataLogin", dataProviderClass = DataProviderLogin.class)
+    public void positiveLogin2(UserDtoLombok user) {
+        app.getUserHelper().loginUserDtoLombok(user);
+        flagLogin = true;
+        flagPopUp = true;
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageSuccessAfterLogin());
+    }
+
+    @Test(dataProvider = "negativePasswordDataLogin", dataProviderClass = DataProviderLogin.class)
+    public void negativePasswordWithoutSymbol(UserDtoLombok user) {
+//        UserDtoLombok userDtoLombok = UserDtoLombok.builder()
+//                .email("testqa20@gmail.com")
+//                .password("123456Aaa")
+//                .build();
+        app.getUserHelper().loginUserDtoLombok(user);
+        flagPopUp = true;
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageLoginIncorrect());
+    }
+
+    @Test(priority = 5)
+    public void negativePasswordWithoutNumbers() {
+        UserDtoLombok userDtoLombok = UserDtoLombok.builder()
+                .email("testqa20@gmail.com")
+                .password("ddsdhjAa$")
+                .build();
+        app.getUserHelper().loginUserDtoLombok(userDtoLombok);
+        flagPopUp = true;
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageLoginIncorrect());
+    }
+
+    @Test(priority = 6)
+    public void negativePasswordWithoutLetters() {
+        UserDtoLombok userDtoLombok = UserDtoLombok.builder()
+                .email("testqa20@gmail.com")
+                .password("12345678$")
+                .build();
+        app.getUserHelper().loginUserDtoLombok(userDtoLombok);
+        flagPopUp = true;
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Assert.assertTrue(app.getUserHelper().validatePopUpMessageLoginIncorrect());
+    }
+}
